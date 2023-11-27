ce6a16e4164361e6523751c431b2a6d08553f762
RadhaSubramonian <radhasshankar@gmail.com>
2023-11-27 11:49:34 +0530

    cardCarouselTopAlbums-qtify-takehome

--- qtify/src/Components/Section/index.js
+++ qtify/src/Components/Section/index.js
@@ -1,10 +1,19 @@
 import './section.css'
 import Card from '../Card'
+import { useState } from 'react';
+import Carousel from '../Carousel';
 
 export default ({ title, data}) => {
+    const [isCollapsed, setIsCollapsed] =  useState(false);
     return (
         <div className='section'>
-            <h1 className='title'>{title}</h1>
+            <div className='section-header'> 
+                <h1 className='title'>{title}</h1>
+                <h1 className='secion-toggle-btn' onClick={()=>{
+                    setIsCollapsed(!isCollapsed)
+                }}>{isCollapsed ? 'Show All' : 'Collapse'}</h1>
+            </div>
+            {isCollapsed ? <Carousel data={data}/> :
             <div className='card-container'>
                 {data.map(cardData => <Card
                             key={cardData.id}
@@ -12,8 +21,8 @@ export default ({ title, data}) => {
                             label = {cardData.title}
                             followersCount={cardData.follows}
                 />)}
-               
             </div>
+            } 
         </div>
     )
 }
